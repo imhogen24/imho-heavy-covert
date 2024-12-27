ebdea9824ca0be76313f3bf5c2b51705133bb823
kennyAnyi9 <kennyanyi9@gmail.com>
2024-12-27 06:53:29 +0000

    build basic layout for the landing page

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,8 +1,13 @@
 'use client';
 
+import { CrossPositinalIcon } from "@/lib/icons";
+
 const HomeLayout = ({ children }:{children: React.ReactNode}) => {
   return(
-    <div>
+
+    <div className=" max-w-screen mx-[10px] md:mx-[11px] lg:max-w-[1200.5px] border border-muted lg:mx-auto relative top-[48px] md:top-[98px] lg:top-[106px] min-h-dvh">
+        {/* making the top container relative makes the cross positional icon show so we keep it that way*/}
+        <CrossPositinalIcon className="hidden lg:block absolute h-6 w-6 -top-3 -left-3" />
       {children}
     </div>
   );
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,10 @@
+import { Hero } from "./_compoennts/hero";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="max-w-screen min-h-dvh flex flex-col justify-center items-center">
 
-
+    <Hero />
 
     </div>
   );
