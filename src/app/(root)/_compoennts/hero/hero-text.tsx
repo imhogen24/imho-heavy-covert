a9a94c399dec1439f0474bdc5c45c667d0068280
kennyAnyi9 <kennyanyi9@gmail.com>
2025-01-07 16:33:45 +0000

    style: improve typography

--- src/app/(root)/_compoennts/hero/hero-text.tsx
+++ src/app/(root)/_compoennts/hero/hero-text.tsx
@@ -8,10 +8,9 @@ export const gradientText = "text-transparent bg-clip-text bg-gradient-to-r from
 const HeroText = () => {
   return (
 
-         <div className="relative flex flex-col items-center justify-center  md:w-[800px] p-5 lg:px-[100px] gap-[16px]">
-          {/* text size for small screens is actually 24px */}
-
-          <h1 className={cn("text-center md:max-w-sm lg:max-w-2xl text-2xl md:text-3xl lg:text-4xl font-[family-name:var(--font-machina)]")}>
+         <div className="relative flex flex-col items-center justify-center  md:w-[800px] p-10">
+            <div className='w-full h-full flex flex-col items-center justify-center gap-[16px]'>
+            <h1 className={cn("text-center text-2xl max-w-lg md:text-3xl lg:text-3xl font-[family-name:var(--font-machina)]")}>
               We{" "}
             <span className={cn(gradientText, "leading-tight")}>
               Design
@@ -21,15 +20,17 @@ const HeroText = () => {
               & Processes
           </h1>
 
-        <Balancer>
+        <Balancer ratio={0.50} preferNative={false}>
           <p className="text-center text-muted-foreground text-md md:text-xl">
              We are a Research and Development company specialized in engineering design. We design and prototype highly robust products and processes.
           </p>
         </Balancer>
 
-          <Button className="w-fit text-secondary bg-black dark:bg-white hover:bg-black/95 mx-auto lg:mx-0 p-[14px] h-[42px] md:h-[48px]" variant={"secondary"}>
+          <Button className="w-fit text-secondary bg-black dark:bg-white hover:bg-black/95 mx-auto lg:mx-0 p-[14px] h-[42px] md:h-[48px] dark:hover:bg-white/85" variant={"secondary"}>
               <ScheduleMeetingIcon/>  Schedule a meeting
           </Button>
+            </div>
+
          </div>
 
 
