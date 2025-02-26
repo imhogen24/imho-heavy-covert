59f18e6c3dba5ff39c278bc5d11432504bd44cfe
kennyAnyi9 <kennyanyi9@gmail.com>
2025-02-26 09:51:28 +0000

    feat: add scroll progress bar on homepage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,10 +5,12 @@ import Projects from "./_compoennts/modules/projects/projects";
 import WorkWithUs from "./_compoennts/modules/work/work";
 import TheTeam from "./_compoennts/modules/team/the-team";
 import Hero from "./_compoennts/modules/hero/hero";
+import { ScrollProgress } from "@/components/magicui/scroll-progress";
 
 export default function Home() {
   return (
     <div className="max-w-screen min-h-dvh flex flex-col overflow-x-hidden">
+      <ScrollProgress className="top-[72px]" />
       <Hero />
       <ImpactSection />
       <CommitedTo />
