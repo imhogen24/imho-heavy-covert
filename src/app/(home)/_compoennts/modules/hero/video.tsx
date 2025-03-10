b0ef4d172063bce72da5fcafd5977d6bb500bc5e
Kennedy Anyidoho <120096792+kennyAnyi9@users.noreply.github.com>
2025-03-10 12:00:09 +0000

    Update video.tsx

--- src/app/(home)/_compoennts/modules/hero/video.tsx
+++ src/app/(home)/_compoennts/modules/hero/video.tsx
@@ -7,14 +7,14 @@ export function HeroVideoDialogDemo() {
         className="dark:hidden block"
         animationStyle="from-center"
         videoSrc="https://res.cloudinary.com/dstrel8mi/video/upload/v1737337135/lv_0_20241008153156_p4rdyp.mp4"
-        thumbnailSrc="https://res.cloudinary.com/dstrel8mi/image/upload/v1735661003/Hero_Video_Thumbnail_e1cb2g.png"
+        thumbnailSrc="https://res.cloudinary.com/dstrel8mi/image/upload/v1741607713/Hero_Video_Thumbnail_e1cb2g.webp"
         thumbnailAlt="Hero Video"
       />
       <HeroVideoDialog
         className="hidden dark:block"
         animationStyle="from-center"
         videoSrc="https://res.cloudinary.com/dstrel8mi/video/upload/v1737337135/lv_0_20241008153156_p4rdyp.mp4"
-        thumbnailSrc="https://res.cloudinary.com/dstrel8mi/image/upload/v1735660830/Hero_Video_Thumbnail--dark_t5a0ib.png"
+        thumbnailSrc="https://res.cloudinary.com/dstrel8mi/image/upload/v1741607975/Hero_Video_Thumbnail--dark_t5a0ib.webp"
         thumbnailAlt="Hero Video"
       />
     </div>
